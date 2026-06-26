cf860e7a725574c118871884334e50d64793ad7b
Peterson Lima <peter.lima104dev@hotmail.com>
2026-06-26 12:31:35 -0300

    adicionado novos produtos

--- js/produtos.js
+++ js/produtos.js
@@ -1,31 +1,58 @@
 const produtos = [
   {
-    id: 1,
+    id: 17,
 
-    nome: "The BlackList",
+    nome: "Dia das Mães",
 
     categoria: "Xícaras",
 
     preco: 39.9,
 
-    descricao: "Caneca da serie The Black List para presentear.",
+    descricao: "Caneca do Dia das Mães para presentear.",
 
     midias: [
       {
         tipo: "imagem",
-        src: "assets/produtos/xicara1/black list 4.jpg",
+        src: "assets/produtos/xicara17/mae 2.png",
       },
       {
         tipo: "imagem",
-        src: "assets/produtos/xicara1/black list 5.jpg",
+        src: "assets/produtos/xicara17/mae 1.png",
       },
       {
         tipo: "imagem",
-        src: "assets/produtos/xicara1/black list 6.jpg",
+        src: "assets/produtos/xicara17/mae 3.png",
       },
       {
         tipo: "imagem",
-        src: "assets/produtos/xicara1/black list 7.jpg",
+        src: "assets/produtos/xicara17/mae 4.png",
+      },
+    ],
+  },
+
+  {
+    id: 18,
+
+    nome: "Grey's Anatomy",
+
+    categoria: "Xícaras",
+
+    preco: 39.9,
+
+    descricao: "Caneca da série Grey's Anatomy para presentear.",
+
+    midias: [
+      {
+        tipo: "imagem",
+        src: "assets/produtos/xicara18/grey 2.png",
+      },
+      {
+        tipo: "imagem",
+        src: "assets/produtos/xicara18/grey 1.png",
+      },
+      {
+        tipo: "imagem",
+        src: "assets/produtos/xicara18/grey 3.png",
       },
     ],
   },
@@ -39,7 +66,7 @@ const produtos = [
 
     preco: 39.9,
 
-    descricao: "Caneca Friends para presentear.",
+    descricao: "Xícara Friends para presentear.",
 
     midias: [
       {
@@ -74,7 +101,7 @@ const produtos = [
 
     preco: 39.9,
 
-    descricao: "Caneca da serie Game of Thrones para presentear.",
+    descricao: "Xícara da serie Game of Thrones para presentear.",
 
     midias: [
       {
@@ -169,7 +196,7 @@ const produtos = [
 
     preco: 39.9,
 
-    descricao: "Caneca do Naruto para presentear.",
+    descricao: "Xícara do Naruto para presentear.",
 
     midias: [
       {
@@ -208,6 +235,37 @@ const produtos = [
   },
 
   {
+    id: 1,
+
+    nome: "The BlackList",
+
+    categoria: "Xícaras",
+
+    preco: 39.9,
+
+    descricao: "Xícara da serie The Black List para presentear.",
+
+    midias: [
+      {
+        tipo: "imagem",
+        src: "assets/produtos/xicara1/black list 4.jpg",
+      },
+      {
+        tipo: "imagem",
+        src: "assets/produtos/xicara1/black list 5.jpg",
+      },
+      {
+        tipo: "imagem",
+        src: "assets/produtos/xicara1/black list 6.jpg",
+      },
+      {
+        tipo: "imagem",
+        src: "assets/produtos/xicara1/black list 7.jpg",
+      },
+    ],
+  },
+
+  {
     id: 5,
 
     nome: "Riverdale",
@@ -216,7 +274,7 @@ const produtos = [
 
     preco: 39.9,
 
-    descricao: "Caneca da serie Riverdale para presentear.",
+    descricao: "Xícara da serie Riverdale para presentear.",
 
     midias: [
       {
@@ -242,6 +300,516 @@ const produtos = [
       {
         tipo: "imagem",
         src: "assets/produtos/xicara5/riverdale 6.jpg",
+      },
+    ],
+  },
+
+  {
+    id: 6,
+
+    nome: "Black Mirror",
+
+    categoria: "Xícaras",
+
+    preco: 39.9,
+
+    descricao: "Xícara da serie Black Mirror para presentear.",
+
+    midias: [
+      {
+        tipo: "imagem",
+        src: "assets/produtos/xicara6/Black Mirror 1.jpg",
+      },
+      {
+        tipo: "imagem",
+        src: "assets/produtos/xicara6/Black Mirror 2.jpg",
+      },
+      {
+        tipo: "imagem",
+        src: "assets/produtos/xicara6/Black Mirror 3.jpg",
+      },
+      {
+        tipo: "imagem",
+        src: "assets/produtos/xicara6/Black Mirror 4.jpg",
+      },
+      {
+        tipo: "imagem",
+        src: "assets/produtos/xicara6/Black Mirror 5.jpg",
+      },
+    ],
+  },
+
+  {
+    id: 7,
+
+    nome: "Breaking Bad",
+
+    categoria: "Xícaras",
+
+    preco: 39.9,
+
+    descricao: "Xícara da serie Breaking Bad para presentear.",
+
+    midias: [
+      {
+        tipo: "imagem",
+        src: "assets/produtos/xicara7/Breaking Bad 1.jpg",
+      },
+      {
+        tipo: "imagem",
+        src: "assets/produtos/xicara7/Breaking Bad 2.jpg",
+      },
+      {
+        tipo: "imagem",
+        src: "assets/produtos/xicara7/Breaking Bad 3.jpg",
+      },
+      {
+        tipo: "imagem",
+        src: "assets/produtos/xicara7/Breaking Bad 4.jpg",
+      },
+      {
+        tipo: "imagem",
+        src: "assets/produtos/xicara7/Breaking Bad 5.jpg",
+      },
+      {
+        tipo: "imagem",
+        src: "assets/produtos/xicara7/Breaking Bad 6.jpg",
+      },
+      {
+        tipo: "imagem",
+        src: "assets/produtos/xicara7/Breaking Bad 7.jpg",
+      },
+      {
+        tipo: "imagem",
+        src: "assets/produtos/xicara7/Breaking Bad 8.jpg",
+      },
+      {
+        tipo: "imagem",
+        src: "assets/produtos/xicara7/Breaking Bad 9.jpg",
+      },
+      {
+        tipo: "imagem",
+        src: "assets/produtos/xicara7/Breaking Bad 10.jpg",
+      },
+    ],
+  },
+
+  {
+    id: 8,
+
+    nome: "Dragon Ball",
+
+    categoria: "Xícaras",
+
+    preco: 39.9,
+
+    descricao: "Xícara Dragon Ball para presentear.",
+
+    midias: [
+      {
+        tipo: "imagem",
+        src: "assets/produtos/xicara8/dragonbool 1.jpg",
+      },
+      {
+        tipo: "imagem",
+        src: "assets/produtos/xicara8/dragonbool 2.jpg",
+      },
+    ],
+  },
+
+  {
+    id: 9,
+
+    nome: "Dia dos Avós",
+
+    categoria: "Xícaras",
+
+    preco: 39.9,
+
+    descricao: "Xícara personalizada para presentear seus avos.",
+
+    midias: [
+      {
+        tipo: "imagem",
+        src: "assets/produtos/xicara9/diaavo 1.png",
+      },
+    ],
+  },
+
+  {
+    id: 10,
+
+    nome: "Dexter",
+
+    categoria: "Xícaras",
+
+    preco: 39.9,
+
+    descricao: "Xícara da serie Dexter para presentear.",
+
+    midias: [
+      {
+        tipo: "imagem",
+        src: "assets/produtos/xicara10/Dexter 1.jpg",
+      },
+      {
+        tipo: "imagem",
+        src: "assets/produtos/xicara10/Dexter 2.jpg",
+      },
+      {
+        tipo: "imagem",
+        src: "assets/produtos/xicara10/Dexter 3.jpg",
+      },
+    ],
+  },
+
+  {
+    id: 11,
+
+    nome: "Diário de um Vampiro",
+
+    categoria: "Xícaras",
+
+    preco: 39.9,
+
+    descricao: "Xícara da série Diário de um Vampiro para presentear.",
+
+    midias: [
+      {
+        tipo: "imagem",
+        src: "assets/produtos/xicara11/dvamp 1.jpg",
+      },
+      {
+        tipo: "imagem",
+        src: "assets/produtos/xicara11/dvamp 2.jpg",
+      },
+      {
+        tipo: "imagem",
+        src: "assets/produtos/xicara11/dvamp 3.jpg",
+      },
+      {
+        tipo: "imagem",
+        src: "assets/produtos/xicara11/dvamp 4.jpg",
+      },
+      {
+        tipo: "imagem",
+        src: "assets/produtos/xicara11/dvamp 5.jpg",
+      },
+      {
+        tipo: "imagem",
+        src: "assets/produtos/xicara11/dvamp 6.jpg",
+      },
+      {
+        tipo: "imagem",
+        src: "assets/produtos/xicara11/dvamp 7.jpg",
+      },
+      {
+        tipo: "imagem",
+        src: "assets/produtos/xicara11/dvamp 8.jpg",
+      },
+      {
+        tipo: "imagem",
+        src: "assets/produtos/xicara11/dvamp 9.jpg",
+      },
+      {
+        tipo: "imagem",
+        src: "assets/produtos/xicara11/dvamp 10.jpg",
+      },
+    ],
+  },
+
+  {
+    id: 12,
+
+    nome: "Fear the Walking Dead",
+
+    categoria: "Xícaras",
+
+    preco: 39.9,
+
+    descricao: "Xícara da série Fear the walking dead para presentear.",
+
+    midias: [
+      {
+        tipo: "imagem",
+        src: "assets/produtos/xicara12/Fear the walking dead 1.jpg",
+      },
+      {
+        tipo: "imagem",
+        src: "assets/produtos/xicara12/Fear the walking dead 2.jpg",
+      },
+      {
+        tipo: "imagem",
+        src: "assets/produtos/xicara12/Fear the walking dead 3.jpg",
+      },
+      {
+        tipo: "imagem",
+        src: "assets/produtos/xicara12/Fear the walking dead 4.jpg",
+      },
+      {
+        tipo: "imagem",
+        src: "assets/produtos/xicara12/Fear the walking dead 5.jpg",
+      },
+      {
+        tipo: "imagem",
+        src: "assets/produtos/xicara12/Fear the walking dead 6.jpg",
+      },
+      {
+        tipo: "imagem",
+        src: "assets/produtos/xicara12/Fear the walking dead 7.jpg",
+      },
+      {
+        tipo: "imagem",
+        src: "assets/produtos/xicara12/Fear the walking dead 8.jpg",
+      },
+      {
+        tipo: "imagem",
+        src: "assets/produtos/xicara12/Fear the walking dead 9.jpg",
+      },
+    ],
+  },
+
+  {
+    id: 13,
+
+    nome: "Flash",
+
+    categoria: "Xícaras",
+
+    preco: 39.9,
+
+    descricao: "Xícara da série Flash para presentear.",
+
+    midias: [
+      {
+        tipo: "imagem",
+        src: "assets/produtos/xicara13/flash 1.jpg",
+      },
+      {
+        tipo: "imagem",
+        src: "assets/produtos/xicara13/flash 2.jpg",
+      },
+      {
+        tipo: "imagem",
+        src: "assets/produtos/xicara13/flash 3.jpg",
+      },
+      {
+        tipo: "imagem",
+        src: "assets/produtos/xicara13/flash 4.jpg",
+      },
+      {
+        tipo: "imagem",
+        src: "assets/produtos/xicara13/flash 5.jpg",
+      },
+      {
+        tipo: "imagem",
+        src: "assets/produtos/xicara13/flash 6.jpg",
+      },
+      {
+        tipo: "imagem",
+        src: "assets/produtos/xicara13/flash 7.jpg",
+      },
+      {
+        tipo: "imagem",
+        src: "assets/produtos/xicara13/flash 8.jpg",
+      },
+      {
+        tipo: "imagem",
+        src: "assets/produtos/xicara13/flash 9.jpg",
+      },
+      {
+        tipo: "imagem",
+        src: "assets/produtos/xicara13/flash 10.jpg",
+      },
+    ],
+  },
+
+  {
+    id: 14,
+
+    nome: "Meu Amor é CHOCO",
+
+    categoria: "Xícaras",
+
+    preco: 49.9,
+
+    descricao: "Xícara Romance CHOCO para presentear.",
+
+    midias: [
+      {
+        tipo: "imagem",
+        src: "assets/produtos/xicara14/romance 1.jpg",
+      },
+      {
+        tipo: "imagem",
+        src: "assets/produtos/xicara14/romance 2.jpg",
+      },
+      {
+        tipo: "imagem",
+        src: "assets/produtos/xicara14/romance 3.jpg",
+      },
+      {
+        tipo: "imagem",
+        src: "assets/produtos/xicara14/romance 4.jpg",
+      },
+    ],
+  },
+
+  {
+    id: 15,
+
+    nome: "Narcos",
+
+    categoria: "Xícaras",
+
+    preco: 39.9,
+
+    descricao: "Caneca da série Narcos para presentear.",
+
+    midias: [
+      {
+        tipo: "imagem",
+        src: "assets/produtos/xicara15/narcos 1.jpg",
+      },
+      {
+        tipo: "imagem",
+        src: "assets/produtos/xicara15/narcos 2.jpg",
+      },
+      {
+        tipo: "imagem",
+        src: "assets/produtos/xicara15/narcos 3.jpg",
+      },
+      {
+        tipo: "imagem",
+        src: "assets/produtos/xicara15/narcos 4.jpg",
+      },
+      {
+        tipo: "imagem",
+        src: "assets/produtos/xicara15/narcos 5.jpg",
+      },
+      {
+        tipo: "imagem",
+        src: "assets/produtos/xicara15/narcos 6.jpg",
+      },
+      {
+        tipo: "imagem",
+        src: "assets/produtos/xicara15/narcos 7.jpg",
+      },
+      {
+        tipo: "imagem",
+        src: "assets/produtos/xicara15/narcos 8.jpg",
+      },
+    ],
+  },
+
+  {
+    id: 16,
+
+    nome: "Netflix",
+
+    categoria: "Xícaras",
+
+    preco: 39.9,
+
+    descricao: "Caneca Netflix para presentear.",
+
+    midias: [
+      {
+        tipo: "imagem",
+        src: "assets/produtos/xicara16/netflix 1.jpg",
+      },
+      {
+        tipo: "imagem",
+        src: "assets/produtos/xicara16/netflix 2.jpg",
+      },
+      {
+        tipo: "imagem",
+        src: "assets/produtos/xicara16/netflix 3.jpg",
+      },
+      {
+        tipo: "imagem",
+        src: "assets/produtos/xicara16/netflix 4.jpg",
+      },
+      {
+        tipo: "imagem",
+        src: "assets/produtos/xicara16/netflix 5.jpg",
+      },
+    ],
+  },
+
+  {
+    id: 19,
+
+    nome: "GTA - Grand Theft Auto",
+
+    categoria: "Xícaras",
+
+    preco: 39.9,
+
+    descricao: "Caneca GTA - Grand Theft Auto para presentear.",
+
+    midias: [
+      {
+        tipo: "imagem",
+        src: "assets/produtos/xicara19/gta 1.png",
+      },
+      {
+        tipo: "imagem",
+        src: "assets/produtos/xicara19/gta 2.png",
+      },
+      {
+        tipo: "imagem",
+        src: "assets/produtos/xicara19/gta 3.png",
+      },
+      {
+        tipo: "imagem",
+        src: "assets/produtos/xicara19/gta 4.png",
+      },
+    ],
+  },
+
+  {
+    id: 20,
+
+    nome: "Call of Duty",
+
+    categoria: "Xícaras",
+
+    preco: 39.9,
+
+    descricao: "Caneca Call of Duty para presentear.",
+
+    midias: [
+      {
+        tipo: "imagem",
+        src: "assets/produtos/xicara20/duty 1.png",
+      },
+      {
+        tipo: "imagem",
+        src: "assets/produtos/xicara20/duty 2.png",
+      },
+      {
+        tipo: "imagem",
+        src: "assets/produtos/xicara20/duty 3.png",
+      },
+    ],
+  },
+
+  {
+    id: 21,
+
+    nome: "Xícara Personalizada",
+
+    categoria: "Xícaras",
+
+    preco: 49.9,
+
+    descricao: "Xícara personalizada",
+
+    midias: [
+      {
+        tipo: "imagem",
+        src: "assets/produtos/xicaraPer/pers.png",
       },
     ],
   },
